bd310225bca4019fabe25f7cbfb7c7a37edc6880
Amy Lyden <amy.m.lyden@gmail.com>
2019-05-20 14:35:07 -0700

    changing gRNA sequences in guidecsv_to_order.sh

--- dashit/contrib/convert_guide_csv_scripts/guidecsv_to_order.sh
+++ dashit/contrib/convert_guide_csv_scripts/guidecsv_to_order.sh
@@ -4,9 +4,10 @@
 # USAGE: bash guidecsv_to_order.sh optimize_guides_output.csv
 
 csv=$(echo $1 | cut -f 1 -d '.')
-tail -n+3 $1 > ${csv}_to_order.csv
-cut -f 2 -d ',' $1 | grep '[ATCG]' | rev | tr ATCG TAGC > revcomp_guide.csv
-paste -d , revcomp_guide.csv ${csv}_to_order.csv > revcomp_guide_full.csv
-printf "guide name,target sequence,rev comp target sequence,guide RNA,guide RNA template with T7 transcription site\ntracrRNA,NA,NA,GACAGCATAGCAAGTTAAAATAAGGCTAGTCCGTTATCAACTTGAAAAAGTGGCACCGAGTCGGTGCTTTTT,AAAAAGCACCGACTCGGTGCCACTTTTTCAAGTTGATAACGGACTAGCCTTATTTTAACTTGCTATGCTGTCCTATAGTGAGTCGTATTA\n" > ${csv}_to_order.csv
-awk -F , -v csv=$csv '{print csv"_crRNA_"$2","$3","$1","$3"GTTTTAGAGCTATGCTGTTTTG,CAAAACAGCATAGCTCTAAAAC"$1"CTATAGTGAGTCGTATTA"}' revcomp_guide_full.csv >> ${csv}_to_order.csv
-rm revcomp_guide_full.csv revcomp_guide.csv
+cut -f 2 -d ',' $1 | tr T U > gRNA.csv
+cut -f 2 -d ',' $1 | rev | tr ATCG TAGC > revcomp_guide.csv
+paste -d , revcomp_guide.csv gRNA.csv ${csv}.csv > revcomp_guide_full.csv
+tail -n+3 revcomp_guide_full.csv > revcomp_guide.csv
+printf "guide name,target sequence,rev comp target sequence,guide RNA,guide RNA template with T7 transcription site\ntracrRNA,NA,NA,GACAGCAUAGCAAGUUAAAAUAAGGCUAGUCCGUUAUCAACUUGAAAAAGUGGCACCGAGUCGGUGCUUUUU,AAAAAGCACCGACTCGGTGCCACTTTTTCAAGTTGATAACGGACTAGCCTTATTTTAACTTGCTATGCTGTCCTATAGTGAGTCGTATTA\n" > ${csv}_to_order.csv
+awk -F , -v csv=$csv '{print csv"_crRNA_"$3","$4","$1","$2"GUUUUAGAGCUAUGCUGUUUUG,CAAAACAGCATAGCTCTAAAAC"$1"CTATAGTGAGTCGTATTA"}' revcomp_guide.csv >> ${csv}_to_order.csv
+rm revcomp_guide_full.csv revcomp_guide.csv gRNA.csv
